ce6e6a7fca2067a26a10ca4b21dd32f331546889
Pramod Jangam <pramod.jangam@deserve.com>
2022-05-22 07:34:07 +0530

    Test get all documents should return 200

--- simple_search/view.py
+++ simple_search/view.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -5,3 +6,8 @@ from rest_framework.views import APIView
 class GreetView(APIView):
     def get(self, request):
         return Response("Hello, World!")
+
+
+class DocumentListView(APIView):
+    def get(self, request):
+        return Response(status=status.HTTP_200_OK)
